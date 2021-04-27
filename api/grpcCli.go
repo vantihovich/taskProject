71e8032b94bb2056616f019fec2ff0a7640021b5
Vadzim Antsikhovich <vadim.antikhovich@gmail.com>
2021-04-28 00:30:24 +0300

    edited the file for establishing grpc connection

--- api/grpcCli.go
+++ api/grpcCli.go
@@ -1,19 +1,21 @@
-package gr
+package grpcCli
 
 import (
-	
 	"log"
+
 	ps "github.com/vantihovich/taskProject/proto"
 	"google.golang.org/grpc"
 )
 
-func gr(){
-	func gRPC_connect() (c ps.GetCredsClient) {
+func gRPC_connect() (c ps.GetCredsClient) {
 	conn, err := grpc.Dial("127.0.0.1:3500", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
-	c := ps.NewGetCredsClient(conn)
+	c = ps.NewGetCredsClient(conn)
 	return c
 }
+
+func grpcCli() {
+	gRPC_connect()
 }
