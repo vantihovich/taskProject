760dec53494e3c7cf8ba70fcff8a80bb05b8b9d5
Vadzim Antsikhovich <vadim.antikhovich@gmail.com>
2021-04-28 00:42:42 +0300

    Fixing grpc connection

--- api/grpcCli.go
+++ api/grpcCli.go
@@ -7,15 +7,11 @@ import (
 	"google.golang.org/grpc"
 )
 
-func gRPC_connect() (c ps.GetCredsClient) {
+func Grpc_connect() (c ps.GetCredsClient) {
 	conn, err := grpc.Dial("127.0.0.1:3500", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
 	c = ps.NewGetCredsClient(conn)
 	return c
-}
-
-func grpcCli() {
-	gRPC_connect()
 }
