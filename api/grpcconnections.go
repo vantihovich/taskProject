c7379ab7f17b1123d9e7b5ae600f2387ae6f828d
Vadzim Antsikhovich <vadim.antikhovich@gmail.com>
2021-06-03 18:59:19 +0300

    Fixed establishing the gRPC connection to server after building protofiles via makefile

--- api/grpcconnections.go
+++ api/grpcconnections.go
@@ -16,16 +16,15 @@ var Cli ps.GetCredsClient
 func GrpcCliConn() (c ps.GetCredsClient) {
 	conn, err := grpc.Dial("127.0.0.1:3500", grpc.WithInsecure())
 	if err != nil {
-		log.Fatal(err)
+		log.WithFields(log.Fields{"error": err}).Panic("Unable to create grpcCliConn:")
 	}
 	Cli = ps.NewGetCredsClient(conn)
 	return Cli
 }
 
 func GrpcServConn() {
-
 	server := grpc.NewServer()
-	instance := new(TokenGeneratorServiceServer)
+	instance := new(ps.UnimplementedGetCredsServer)
 	ps.RegisterGetCredsServer(server, instance)
 
 	listener, err := net.Listen("tcp", ":3500")
@@ -36,13 +35,13 @@ func GrpcServConn() {
 	if err = server.Serve(listener); err != nil {
 		log.WithFields(log.Fields{"error": err}).Panic("Unable to start server")
 	}
-
+	fmt.Println("Struct:", instance)
 }
 
-type TokenGeneratorServiceServer struct {
+type UnimplementedGetCredsServer struct {
 }
 
-func (s TokenGeneratorServiceServer) GenerateToken(c context.Context, req *ps.Request) (*ps.Response, error) {
+func (u UnimplementedGetCredsServer) GenerateToken(c context.Context, req *ps.Request) (*ps.Response, error) {
 	c = context.Background()
 	var err error
 	response := new(ps.Response)
@@ -63,4 +62,4 @@ func (s TokenGeneratorServiceServer) GenerateToken(c context.Context, req *ps.Re
 	return response, err
 }
 
-func (s TokenGeneratorServiceServer) mustEmbedUnimplementedGetCredsServer() {}
+func (u UnimplementedGetCredsServer) mustEmbedUnimplementedGetCredsServer() {}
